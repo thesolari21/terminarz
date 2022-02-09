8f4c506a3fdfcbc797696362527bd99baadb8506
Bartek Solarski <bartsol21@gmail.com>
2022-02-09 20:52:06 +0100

    first commit load teams from file

---
+++ terminarz.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+path = "teams.txt"
+f =open(path, "r", encoding="utf-8")
+
+for line in f:
+    print(line, end="")
+
+
+
+
